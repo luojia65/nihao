575d3531b94db3273d3da374358cbf14b229af22
luojia65 <me@luojia.cc>
2019-04-28 00:19:32 +0800

    Rename

--- nihao-usb/examples/size_of_handle.rs
+++
@@ -1,7 +0,0 @@
-// use nihao_usb::sys::windows::*;
-// use core::mem::size_of;
-
-// fn main() {
-//     println!("Size of InterfaceHandle: {}", size_of::<usb::InterfaceHandle>());
-//     println!("Size of Option<InterfaceHandle>: {}", size_of::<Option<usb::InterfaceHandle>>());
-// }
