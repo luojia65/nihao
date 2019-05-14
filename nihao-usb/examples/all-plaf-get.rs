e7215fe9da45366d65eba98e130c8d408a355d10
luojia65 <me@luojia.cc>
2019-05-14 20:27:31 +0800

    Oops

--- nihao-usb/examples/all-plaf-get.rs
+++ nihao-usb/examples/all-plaf-get.rs
@@ -3,8 +3,6 @@ use std::io;
 fn main() -> io::Result<()> {
     for device in nihao_usb::devices()? {
         if let Ok(handle) = device?.open() {
-            println!("begin");
-            std::thread::sleep(std::time::Duration::from_secs(2));
             println!("{:?}", handle.device_descriptor());
         }
     }
