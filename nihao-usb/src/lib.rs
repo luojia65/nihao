0dae538c89f1716d0c3ab31619cf0ad7dac01f67
luojia65 <me@luojia.cc>
2019-05-30 14:03:25 +0800

    Interface descriptor

--- nihao-usb/src/lib.rs
+++ nihao-usb/src/lib.rs
@@ -100,3 +100,16 @@ pub struct DeviceDescriptor {
     pub serial_number: u8,
     pub num_configurations: u8,
 }
+
+/// An `InterfaceDescriptor` describing what this name represents in the USB specification
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+pub struct InterfaceDescriptor {
+    pub length: u8,
+    pub descriptor_type: u8, 
+    pub interface_number: u8,
+    pub alternate_setting: u8,
+    pub num_endpoints: u8,
+    pub interface_class: u8,
+    pub interface_protocol: u8,
+    pub index_interface: u8,
+}
