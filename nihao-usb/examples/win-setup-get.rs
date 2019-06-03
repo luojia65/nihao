548e71e10ab4dcc2b431bb0591ea78863eac1ed3
luojia65 <me@luojia.cc>
2019-06-03 19:32:58 +0800

    Fix example

--- nihao-usb/examples/win-setup-get.rs
+++ nihao-usb/examples/win-setup-get.rs
@@ -5,10 +5,12 @@ fn main() -> io::Result<()> {
     let info_handle = ListOptions::all_usb_interfaces()
         .present()
         .list()?;
-    info_handle.iter()
-        .filter_map(|info| info.ok())
-        .map(|info| info.open())
-        .filter_map(|usb| usb.ok())
-        .for_each(|usb| println!("{:?}", usb.device_descriptor()));
+    for info in info_handle.iter() {
+        if let Ok(info) = info {
+            if let Ok(usb) = info.open() {
+                println!("{:?}", usb.device_descriptor());
+            }
+        }
+    }
     Ok(())
 }
